abd31e440c02e9d52d2c082317dbaa13dc84e618
Harmish Patel <harmishpatel1996@gmail.com>
2024-07-24 23:22:24 +0530

    get all channels route added

--- src/controller/ChannelCotroller.ts
+++ src/controller/ChannelCotroller.ts
@@ -2,112 +2,122 @@ import { Request, Response } from "express";
 import { handleErrors } from "./AuthController";
 import { ChannelModel } from "../models/ChannelModel";
 
-export const getAllChannels = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const channels = await ChannelModel.find({ participants: { $in: [id] } });
-    res.send({ channels });
-  } catch (error) {
-    return res.status(404).send(handleErrors(error));
-  }
+export const getChannels = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const channels = await ChannelModel.find({ participants: { $in: [id] } });
+		res.send({ channels });
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
 };
+
 export const postChannel = async (req: Request, res: Response) => {
-  try {
-    const { channel_name, is_private } = req.body;
-    const channel = new ChannelModel({
-      channel_name,
-      is_private,
-    });
-    channel.save();
-    res.send({
-      channel,
-      message: "Channel created succesfully",
-      success: true,
-    });
-  } catch (error) {
-    return res.status(404).send(handleErrors(error));
-  }
+	try {
+		const { channel_name, is_private } = req.body;
+		const channel = new ChannelModel({
+			channel_name,
+			is_private,
+		});
+		channel.save();
+		res.send({
+			channel,
+			message: "Channel created succesfully",
+			success: true,
+		});
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
 };
 
 export const updateChannel = async (req: Request, res: Response) => {
-  try {
-    const { channel_name, is_private, channel_id, channel_theme } = req.body;
+	try {
+		const { channel_name, is_private, channel_id, channel_theme } = req.body;
 
-    const channel = await ChannelModel.findByIdAndUpdate(
-      { _id: channel_id },
-      {
-        $set: {
-          channel_name,
-          is_private,
-          channel_theme,
-        },
-      },
-      { new: true }
-    );
-    res.send({
-      channel,
-      message: "Channel updated succesfully",
-      success: true,
-    });
-  } catch (error) {
-    return res.status(404).send(handleErrors(error));
-  }
+		const channel = await ChannelModel.findByIdAndUpdate(
+			{ _id: channel_id },
+			{
+				$set: {
+					channel_name,
+					is_private,
+					channel_theme,
+				},
+			},
+			{ new: true }
+		);
+		res.send({
+			channel,
+			message: "Channel updated succesfully",
+			success: true,
+		});
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
 };
 
 export const deleteChannel = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-    await ChannelModel.deleteOne({ _id: id });
-    res.send({
-      message: "Channel deleted succesfully",
-      success: true,
-    });
-  } catch (error) {
-    return res.status(404).send(handleErrors(error));
-  }
+		await ChannelModel.deleteOne({ _id: id });
+		res.send({
+			message: "Channel deleted succesfully",
+			success: true,
+		});
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
 };
 
 export const postParticipants = async (req: Request, res: Response) => {
-  try {
-    const { participants, channel_id } = req.body;
+	try {
+		const { participants, channel_id } = req.body;
 
-    const channel = await ChannelModel.findByIdAndUpdate(
-      { _id: channel_id },
-      {
-        $addToSet: { participants },
-      },
-      { new: true }
-    );
-    res.send({
-      channel,
-      message: "Particpant added succesfully",
-      success: true,
-    });
-  } catch (error) {
-    return res.status(404).send(handleErrors(error));
-  }
+		const channel = await ChannelModel.findByIdAndUpdate(
+			{ _id: channel_id },
+			{
+				$addToSet: { participants },
+			},
+			{ new: true }
+		);
+		res.send({
+			channel,
+			message: "Particpant added succesfully",
+			success: true,
+		});
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
 };
 
 export const deleteParticipant = async (req: Request, res: Response) => {
-  try {
-    const { id, channel_id } = req.params;
+	try {
+		const { id, channel_id } = req.params;
 
-    const channel = await ChannelModel.findByIdAndUpdate(
-      { _id: channel_id },
-      {
-        $pullAll: {
-          participants: [id],
-        },
-      },
-      { new: true }
-    );
-    res.send({
-      channel,
-      message: "Participant deleted succesfully",
-      success: true,
-    });
-  } catch (error) {
-    return res.status(404).send(handleErrors(error));
-  }
+		const channel = await ChannelModel.findByIdAndUpdate(
+			{ _id: channel_id },
+			{
+				$pullAll: {
+					participants: [id],
+				},
+			},
+			{ new: true }
+		);
+		res.send({
+			channel,
+			message: "Participant deleted succesfully",
+			success: true,
+		});
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
+};
+
+export const getAllChannels = async (req: Request, res: Response) => {
+	try {
+		const channels = await ChannelModel.find();
+		res.send({ channels });
+	} catch (error) {
+		return res.status(404).send(handleErrors(error));
+	}
 };
--- src/routes/Channel.ts
+++ src/routes/Channel.ts
@@ -9,11 +9,12 @@ import {
   postChannel,
   postParticipants,
   updateChannel,
+  getChannels
 } from "../controller/ChannelCotroller";
 import { Router } from "express";
 
 const router = Router();
-router.get("/get/channels/:id", getAllChannels);
+router.get("/get/channels/:id", getChannels);
 router.post("/add/channel", postChannel);
 router.put("/update/channel", updateChannel);
 router.delete("/delete/:id", deleteChannel);
@@ -21,5 +22,6 @@ router.post("/add/participants", postParticipants);
 router.delete("/delete/:channel_id/:id", deleteParticipant);
 router.post("/channel/message", postChannelMessage);
 router.get("/channel/messages/:channel_id", getChannelMessage);
+router.get("/get/all/channels", getAllChannels);
 
 export default router;
